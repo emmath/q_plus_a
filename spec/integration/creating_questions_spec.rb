5f3fd4403ff62657b627a470dfb85cf12bb090a3
emmath <emmatrevena@gmail.com>
2013-07-22 15:06:09 -0700

    Add title functionality for show page

--- spec/integration/creating_questions_spec.rb
+++ spec/integration/creating_questions_spec.rb
@@ -8,5 +8,9 @@ feature 'Creating Questions' do
     fill_in 'Description', :with => "A question pertaining to animals"
     click_button 'Create Question'
     page.should have_content('Question has been created.')
+    question = Question.find_by_topic("Animal Kingdom")
+    page.current_url.should == question_url(question)
+    title = "Animal Kingdom - Questions - Q_Plus_A"
+    find("title").should have_content(title)
   end
 end
