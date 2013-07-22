ad7aa4768e3682e2c0157809380b6b1b290bb592
emmath <emmatrevena@gmail.com>
2013-07-22 13:37:22 -0700

    Generate Q Plus A application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QPlusA::Application.config.secret_token = 'df6f0008a427122da3ba5a740a62d95d4e1741b0f003b778e04a91b1d59227dbf923e022920c18e6ed784b036a0fac3dd1701e3ac8342fd4a6e2616070c08060'
